03b29a48c030cdfa59597baee2410e2d1db7c044
Baran <brian.baran@torontofilmschool.ca>
2020-01-06 11:00:40 -0500

    First Jump Iteration
    Jump in progress

--- IronlightCode/Assets/DEBUG_AREA/JUMP_DEBUG/JumpTest.cs
+++ IronlightCode/Assets/DEBUG_AREA/JUMP_DEBUG/JumpTest.cs
@@ -7,32 +7,72 @@ public class JumpTest : MonoBehaviour
     float ver;
     float hor;
     bool jumpInput = false;
-
-
+    public Rigidbody rb;
+    bool isRising;
+    bool isFalling;
     public Vector3 moveDir;
-
+    public float jumpHeight = 80;
+    Vector3 jumpVelocity;
+    Vector3 fallVelocity;
+    public float fallSpeed = 18;
+    public float jumpForce = 200;
+    Vector3 currPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isRising = false;
+        isFalling = false;
+        jumpVelocity.y = jumpForce;
+        fallVelocity.y = fallSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currPos = rb.position;
         GetInput();
 
         moveDir = new Vector3(hor, 0, ver);
 
         if (jumpInput)
         {
-            Jump();
+            isRising = true;
+            if (isRising && rb.position.y <= jumpHeight)
+            {
+                Jump();
+                if (rb.position.y >= jumpHeight)
+                {
+                    isRising = false;
+
+                    isFalling = true;
+                }
+
+            }
+            if (isFalling)
+            {
+                Fall();
+                if (rb.position.y <= 1)
+                {
+                    print("landed");
+                    isFalling = false;
+                    jumpInput = false;
+                    currPos.y = 1;
+                }
+            }
         }
+
     }
-    
+
     void Jump()
     {
         Debug.Log("I'm Jumping bitches");
+
+        rb.position += jumpVelocity * Time.deltaTime;
+
+    }
+    void Fall()
+    {
+        rb.position -= fallVelocity * Time.deltaTime;
     }
 
     void GetInput()
@@ -44,9 +84,6 @@ public class JumpTest : MonoBehaviour
         {
             jumpInput = true;
         }
-        else
-        {
-            jumpInput = false;
-        }
+       
     }
 }
