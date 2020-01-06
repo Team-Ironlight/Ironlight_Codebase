d2beb52e8f20cce77a7f4b698c12ff7d6a07f42a
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-06 10:03:30 -0500

    Added Dodge debug area

--- IronlightCode/Assets/DEBUG_AREA/DODGE_DEBUG/DodgeTest.cs
+++ IronlightCode/Assets/DEBUG_AREA/DODGE_DEBUG/DodgeTest.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class DodgeTest : MonoBehaviour
 {
+    float ver;
+    float hor;
+    bool dodgeInput = false;
+
+
+    public Vector3 moveDir;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+
+        moveDir = new Vector3(hor, 0, ver);
+
+        if (dodgeInput)
+        {
+            Dodge();
+        }
+    }
+
+    void Dodge()
+    {
+        Debug.Log("I'm Jumping bitches");
+    }
+
+    void GetInput()
+    {
+        ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dodgeInput = true;
+        }
+        else
+        {
+            dodgeInput = false;
+        }
     }
 }
