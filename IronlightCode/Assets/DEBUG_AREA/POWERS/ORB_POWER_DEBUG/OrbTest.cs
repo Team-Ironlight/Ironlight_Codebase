22b5ce08a56131b19271ee7a0bc53a5d0b8fd7a1
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-06 08:34:06 -0500

    Added testing areas

---
+++ IronlightCode/Assets/DEBUG_AREA/POWERS/ORB_POWER_DEBUG/OrbTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbTest : MonoBehaviour
+{
+    public bool inputReceived = false;
+
+
+    private void Update()
+    {
+        GetInput();
+        if (inputReceived)
+            Shoot();
+    }
+
+    void GetInput()
+    {
+        // Change this depending on how you want the attack to work
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inputReceived = true;
+        }
+        else
+        {
+            inputReceived = false;
+        }
+    }
+
+    // Code to perform attack
+    void Shoot()
+    {
+        Debug.Log("Bitches");
+    }
+}
