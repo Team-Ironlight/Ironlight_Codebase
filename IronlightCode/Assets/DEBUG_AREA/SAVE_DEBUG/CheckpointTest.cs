bd700bee49e9236b39f822b765d9d059d0a6539c
makughini <45440375+makughini@users.noreply.github.com>
2020-01-06 13:42:57 -0500

    Debug Check Point

--- IronlightCode/Assets/DEBUG_AREA/SAVE_DEBUG/CheckpointTest.cs
+++ IronlightCode/Assets/DEBUG_AREA/SAVE_DEBUG/CheckpointTest.cs
@@ -6,15 +6,23 @@ public class CheckpointTest : MonoBehaviour
 {
     float ver;
     float hor;
-    bool inputeRecieved = false;
+    bool inputRecieved = false;
+
+    public GameObject currentCheck;
 
     public Vector3 moveDir;
+
+    public static GameObject[] CheckPointsList;
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // We search all the checkpoints in the current scene
+        CheckPointsList = GameObject.FindGameObjectsWithTag("CheckPoint");
+
     }
 
     // Update is called once per frame
@@ -23,7 +31,7 @@ public class CheckpointTest : MonoBehaviour
         GetInput();
         Move();
 
-        if (inputeRecieved)
+        if (inputRecieved)
         {
             ResetToCheckpoint();
         }
@@ -36,16 +44,40 @@ public class CheckpointTest : MonoBehaviour
 
         moveDir = new Vector3(hor, 0, ver);
 
-        inputeRecieved = Input.GetKeyDown(KeyCode.Space);
+        inputRecieved = Input.GetKeyDown(KeyCode.Space);
     }
 
     void ResetToCheckpoint()
     {
         // move object to las activated checkpoint
+        gameObject.transform.position = currentCheck.transform.position;
     }
 
     void Move()
     {
         transform.position = new Vector3(transform.position.x + moveDir.x, 0, 0);
     }
+
+    // when checkpoint trigger entered
+        // assign checkpoint gameobject to currentCheck
+        // deactivate checkpoint gameobject's trigger
+
+
+    // when player dies OR when input received
+        // set player position to currentCheck position
+
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If the player passes through the checkpoint, we activate it
+        if (other.tag == "CheckPoint")
+        {
+            currentCheck = other.gameObject;
+        }
+    }
+
+   
+    
 }
