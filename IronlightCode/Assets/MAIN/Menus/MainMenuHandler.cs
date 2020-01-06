93591bf1be1a509a9e9cd112418f185ca89e9b43
Rhonielvp <48013641+Rhonielvp@users.noreply.github.com>
2020-01-06 16:43:48 -0500

    Menu done

--- IronlightCode/Assets/MAIN/Menus/MainMenuHandler.cs
+++ IronlightCode/Assets/MAIN/Menus/MainMenuHandler.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{  
+    public void NewGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+        SceneManager.LoadScene(1);     
+    } 
+    
+    public void Option()
     {
-        
+        SceneManager.LoadScene(2);     
+    } 
+    
+    public void Quit()
+    {
+        // SceneManager.LoadScene(3);
+        Debug.Log("Quit Game");
     }
 }
