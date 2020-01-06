000702c537a51853c3468a2fc0b5aeca71f4b5e8
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-06 10:50:42 -0500

    Added main menu and checkpoint debug

---
+++ IronlightCode/Assets/DEBUG_AREA/SAVE_DEBUG/CheckpointTest.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointTest : MonoBehaviour
+{
+    float ver;
+    float hor;
+    bool inputeRecieved = false;
+
+    public Vector3 moveDir;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+
+        if (inputeRecieved)
+        {
+            ResetToCheckpoint();
+        }
+    }
+
+    void GetInput()
+    {
+        ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+
+        moveDir = new Vector3(hor, 0, ver);
+
+        inputeRecieved = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    void ResetToCheckpoint()
+    {
+        // move object to las activated checkpoint
+    }
+
+    void Move()
+    {
+        transform.position = new Vector3(transform.position.x + moveDir.x, 0, 0);
+    }
+}
