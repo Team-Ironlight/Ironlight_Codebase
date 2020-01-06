cba061b1343e2b82d013e576087f1b3ee4ebd85a
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-06 09:56:17 -0500

    Added Jump debug area

---
+++ IronlightCode/Assets/DEBUG_AREA/JUMP_DEBUG/JumpTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpTest : MonoBehaviour
+{
+    float ver;
+    float hor;
+    bool jumpInput = false;
+
+
+    public Vector3 moveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+
+        moveDir = new Vector3(hor, 0, ver);
+
+        if (jumpInput)
+        {
+            Jump();
+        }
+    }
+    
+    void Jump()
+    {
+        Debug.Log("I'm Jumping bitches");
+    }
+
+    void GetInput()
+    {
+        ver = Input.GetAxis("Vertical");
+        hor = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpInput = true;
+        }
+        else
+        {
+            jumpInput = false;
+        }
+    }
+}
