b67a873f48906f9aef83d1a894a81603d62a27d5
rubhen925 <48264153+rubhen925@users.noreply.github.com>
2020-01-06 16:35:32 -0500

    radial attack has 3 modes done
    pulse, hold, and charge added.

--- IronlightCode/Assets/DEBUG_AREA/POWERS/RADIAL_POWER_DEBUG/RadialTest.cs
+++ IronlightCode/Assets/DEBUG_AREA/POWERS/RADIAL_POWER_DEBUG/RadialTest.cs
@@ -6,37 +6,138 @@ public class RadialTest : MonoBehaviour
 {
     public bool inputReceived = false;
     public float radius;
+    public float radiusMax = 1f;
+    public float radiusSpeed = 1f;
 
-    public bool pulseMode = false;
-    public bool holdMode = false;
-    public bool chargeMode = false;
+   
+    public bool isPressed = false;
 
+    public float timer = 5.0f;
+    private float countdown = 0.01f;
+
+    private enum modes { off, pulse, hold, charge }
+    private modes currentMode = modes.off; 
 
     private void Update()
     {
-        GetInput();
-        if (inputReceived)
-            Shoot();
+        //GetInput();
+        //if (inputReceived)
+        Shoot();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentMode = modes.pulse;
+            Debug.Log("Pulse Mode Active");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentMode = modes.hold;
+            Debug.Log("Hold Mode Active");
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentMode = modes.charge;
+            Debug.Log("Charge Mode Active");
+        }
     }
 
-    void GetInput()
+    //void GetInput()
+    //{
+    //    // Change this depending on how you want the attack to work
+        
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        inputReceived = true;
+    //    }
+    //    else
+    //    {
+    //        inputReceived = false;
+    //        radius = 0.01f;
+    //    }
+    //}
+
+    IEnumerator RadialAction(float x)
     {
-        // Change this depending on how you want the attack to work
+        Debug.Log("PULSE");
+        if(x < 0)
+        {
+            x = 1f;
+        }
+
+        float count = 0.0f;
+        while(count < x)
+        {
+            radius = count;
+            count += Time.deltaTime;
+
+            yield return null;
+        }
+
+        radius = 0.01f;
+        coroutineOn = false;
+    }
+
+    Coroutine currentCoroutine = null;
+    bool coroutineOn = false;
+    float chargeCount = 0f;
+
+    private void Pulse()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && coroutineOn == false)
+        {
+            coroutineOn = true;
+            currentCoroutine = StartCoroutine(RadialAction(radiusMax));
+        }
+    }
+
+    private void Hold()
+    {
         if (Input.GetKey(KeyCode.Space))
         {
-            inputReceived = true;
+            if (radius < radiusMax)
+            {
+                radius += radiusSpeed * Time.deltaTime;
+            }
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-            inputReceived = false;
             radius = 0.01f;
+        }
+    }
+
+    private void Charge()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (chargeCount < radiusMax)
+            {
+                chargeCount += radiusSpeed * Time.deltaTime;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            currentCoroutine = StartCoroutine(RadialAction(chargeCount));
         }
     }
 
     // Code to perform attack
     void Shoot()
     {
-        radius += 0.5f * Time.deltaTime;
+        Debug.Log("CurrentMode: " + currentMode.ToString());
+        if(currentMode == modes.pulse)
+        {
+            Pulse();
+        }
+        else if(currentMode == modes.hold)
+        {
+            Hold();
+        }
+        else if(currentMode == modes.charge)
+        {
+            Charge();
+        }
     }
 
     private void OnDrawGizmos()
