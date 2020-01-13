8844bc06c463c04266465a4fa63dfda2763d2b22
Wasiq Omar <40704271+AquaAno@users.noreply.github.com>
2020-01-13 11:14:48 -0800

    OrbDeflect Done!

---
+++ IronlightCode/Assets/TESTING/PhysicsTest/OrbCollisionDeflect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbCollisionDeflect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.Reflect(this.GetComponent<Rigidbody>().position, this.GetComponent<Rigidbody>().position) *15, ForceMode.Impulse);
+    }
+}
