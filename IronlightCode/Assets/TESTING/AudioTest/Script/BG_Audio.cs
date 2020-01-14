fd084b60e81bb5797f9c991d392abff8545df177
Rhonielvp <48013641+Rhonielvp@users.noreply.github.com>
2020-01-14 17:30:26 -0500

    5:30 Audio sound update

---
+++ IronlightCode/Assets/TESTING/AudioTest/Script/BG_Audio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// What it requires.
+[RequireComponent(typeof(AudioSource))]
+
+public class BG_Audio : MonoBehaviour
+{
+    // Access the Audio source.
+    AudioSource MyAudioSource;
+
+    // Background, Combat, Transition Music. 
+    public AudioClip M_1;
+    public AudioClip C_1;
+    public AudioClip T_1;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
