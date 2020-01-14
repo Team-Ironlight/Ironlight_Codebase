456189c13cbd7998042ccb2f4c26080f5ac64cbe
PhilJamesLapuz <Phil-James_Lapuz@outlook.com>
2020-01-14 14:39:21 -0500

    Version 1 ( for Squirrel Trello Card)
    For this Version 1 :
    * AI System(pluggable States)
    * Template for Patrol State, Wander State, Flee State, Chase State and Attack State
    * were using NavMesh for this version
    * Instruction/Setup was written at the StateMachine Script
    * Scripts are:
        - StateMachine, PatrolState, WanderState, FleeState, ChaseState, AttackState

---
+++ IronlightCode/Assets/TESTING/EnemyTestArea/SquirrelTest/Scripts/States/ChaseState.cs
@@ -0,0 +1,63 @@
+﻿//Programmer : Phil James
+using UnityEngine;
+using System.Collections;
+using IronLight;
+
+[System.Serializable]
+public class ChaseState : StateMachine.BaseState
+{
+    [Header("Target")]
+	public Transform target;
+    [Header("Decision Making")]
+    public float dampTime = 1.25f;
+	private Vector3 velocity = Vector3.zero;
+	public float minDistanceToTarget = 1.0f;
+	public string OnEnemyLostState = "FleeState";                       //To Do:  Convert this to enum
+    public string OnEnemyMinDistanceState = "FleeState";                //To Do:  Convert this to enum
+    [Header("-------------")]
+    [Header("Movement")]
+    public float walk_Speed = 2f;
+    public float run_Speed = 4f;
+
+    private UnityEngine.AI.NavMeshAgent agent;   //reference to the navmesh agent.
+
+
+    public override void  OnEnter()
+	{
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+	public override void  Tick()
+	{
+		if(target != null)
+        {
+            // enable the agent to move again
+            agent.isStopped = false;
+            agent.speed = run_Speed;
+
+            Vector3 delta = target.position - transform.position;
+			Vector3 destination = transform.position + delta;
+        //    agentUpdate = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+
+            agent.SetDestination(destination);
+		}
+	}
+	public override string CheckConditions()
+	{
+		
+		if(target == null)
+		{
+           
+			return OnEnemyLostState;
+		}
+		else if(Vector3.Distance(transform.position, target.position) < minDistanceToTarget)
+		{
+			return OnEnemyMinDistanceState;
+		}
+		else
+			return "";
+	}
+	public override void   OnExit()
+	{
+		// TODO destroy Effects / Animation
+	}
+}
