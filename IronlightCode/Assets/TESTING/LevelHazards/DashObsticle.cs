972b9c7ddd02f0c2f7595f567c76d2fe699e3d8c
Baran <brian.baran@torontofilmschool.ca>
2020-01-14 16:29:10 -0500

    DartingObstacleUpdate

--- IronlightCode/Assets/TESTING/LevelHazards/DashObsticle.cs
+++ IronlightCode/Assets/TESTING/LevelHazards/DashObsticle.cs
@@ -9,11 +9,13 @@ public class DashObsticle : MonoBehaviour
     public GameObject markerTwo;
     bool darting = false;
     public float speed=5;
+    bool goMarkOne;
+
     // Start is called before the first frame update
     void Start()
     {
         Enemy.transform.position = markerOne.transform.position;
-        darting = true;
+
 
     }
 
@@ -29,13 +31,41 @@ public class DashObsticle : MonoBehaviour
     void Dart()
     {
         print("Move bitch");
-        if(Enemy.transform.position.z <= markerOne.transform.position.z)
-        Enemy.transform.LookAt(markerTwo.transform);
-        Enemy.transform.Translate(Vector3.forward * Time.deltaTime*speed);
-        if (Enemy.transform.position.z >= markerTwo.transform.position.z)
+        if (Enemy.transform.position.z <= markerOne.transform.position.z)
+        {
+            Enemy.transform.LookAt(markerTwo.transform);
+            goMarkOne = false;
+        }
+            Enemy.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+            if (Enemy.transform.position.z >= markerTwo.transform.position.z)
         {
             Enemy.transform.LookAt(markerOne.transform.position);
+            goMarkOne = true;
             Enemy.transform.Translate(Vector3.forward * Time.deltaTime*speed);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        darting = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            returnHome();
+        }
+    }
+    void returnHome()
+    {
+        if (goMarkOne)
+        {
+            Enemy.transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            if(!goMarkOne)
+            {
+                darting = false;
+            }
         }
     }
 }
