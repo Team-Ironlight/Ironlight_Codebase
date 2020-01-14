f29ca804a7de76f749c869524f1d6e97dea0be37
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-14 15:53:34 -0500

    created Player power test prefab
    Prefab with current progress of powers loaded.
    Used for testing power interactions

---
+++ IronlightCode/Assets/TESTING/PlayerTestArea/TEST_PowerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_PowerController : MonoBehaviour
+{
+    public PLY_OrbTest orbTest;
+    public PLY_BeamTest beamTest;
+    public PLY_ImanBlastTest blastTest;
+    public GameObject firePoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firePoint = GameObject.FindGameObjectWithTag("Muzzle");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
