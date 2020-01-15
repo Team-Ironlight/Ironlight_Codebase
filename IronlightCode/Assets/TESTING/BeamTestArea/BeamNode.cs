f06d068405fc199c92d885a499a3d1386f967228
WolfHandss <48457883+WolfHandss@users.noreply.github.com>
2020-01-14 19:18:20 -0500

    new beam

---
+++ IronlightCode/Assets/TESTING/BeamTestArea/BeamNode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeamNode : MonoBehaviour
+{
+    private PLY_3rdBeam BeamAccess;
+
+    private GameObject NextPoint;
+
+    private int nextNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeamAccess = GameObject.FindGameObjectWithTag("Beam").GetComponent<PLY_3rdBeam>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnNext()
+    {
+        NextPoint = Instantiate(gameObject, transform.position, transform.rotation);
+        BeamAccess.beamLists.Add(NextPoint);
+    }
+
+    public void AdjustNextNode()
+    {
+    }
+}
