a84c253eef0e19e8f3b033235dececc27e063943
Baran <brian.baran@torontofilmschool.ca>
2020-01-21 13:06:03 -0500

    HealthAndSpiritComponent

--- IronlightCode/Assets/TESTING/PlayerTestArea/TEST_PowerController.cs
+++ IronlightCode/Assets/TESTING/PlayerTestArea/TEST_PowerController.cs
@@ -9,12 +9,13 @@ public class TEST_PowerController : MonoBehaviour
     public PLY_ImanBlastTest blastTest;
     public GameObject firePoint;
 
-    float Spirit; //Place holder for spiritBar
+    public PLY_HealthComponent Spirit;
+
     // Start is called before the first frame update
     void Start()
     {
         firePoint = GameObject.FindGameObjectWithTag("Muzzle");
-        Spirit = 100;
+
     }
 
     // Update is called once per frame
@@ -28,24 +29,48 @@ public class TEST_PowerController : MonoBehaviour
     {
         if (orbTest.shotFired == true)
         {
-            Spirit -= 5;
-            print("Spirit Remaining: " + Spirit);
+            if (Spirit.CurrMana > 0)
+            {
+                Spirit.SubManaInst(5);
+                print("Spirit Remaining: " + Spirit.CurrMana);
+            }
+            else if (Spirit.CurrMana <= 0)
+            {
+                Spirit.SubHealth(5);
+                print("Health Remaining: " + Spirit.currentHealth);
+            }
         }
     }
     void BeamDrain()
     {
         if (beamTest.StartAttack == true)
         {
-            Spirit -= 2 * Time.deltaTime;
-            print("Spirit Remaining: " + Spirit);
+            if (Spirit.CurrMana > 0)
+            {
+                Spirit.SubManaTime(2);
+                print("Spirit Remaining: " + Spirit.CurrMana);
+            }
+            else if (Spirit.CurrMana <= 0)
+            {
+                Spirit.SubHealth(5);
+                print("Health Remaining: " + Spirit.currentHealth);
+            }
         }
     }
     void BlastDrain()
     {
         if(blastTest.drainSpirit == true)
         {
-            Spirit -= 5 * Time.deltaTime;
-            print("Spirit Remaining: " + Spirit);
+            if (Spirit.CurrMana > 0)
+            {
+                Spirit.SubManaTime(5);
+                print("Spirit Remaining: " + Spirit.CurrMana);
+            }
+            else if (Spirit.CurrMana <= 0)
+            {
+                Spirit.SubHealth(5);
+                print("Health Remaining: " + Spirit.currentHealth);
+            }
         }
 
     }
