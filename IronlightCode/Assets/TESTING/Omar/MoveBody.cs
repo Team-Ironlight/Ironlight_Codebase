e08323755e4f97251fa553ddc1c4da5be6672552
A00425164 <40704271+A00425164@users.noreply.github.com>
2020-01-27 13:09:32 -0500

    buildtestMovement

---
+++ IronlightCode/Assets/TESTING/Omar/MoveBody.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBody : MonoBehaviour
+{
+	Vector3 movement;
+	float moveX;
+	float moveZ;
+	public float speed = 15;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		MoveObject();
+	}
+
+
+	void MoveObject()
+	{
+		moveX = Input.GetAxis("Horizontal");
+		moveZ = Input.GetAxis("Vertical");
+		movement = new Vector3(moveX, 0, moveZ);
+		transform.position += movement * Time.deltaTime * speed;
+	}
+}
