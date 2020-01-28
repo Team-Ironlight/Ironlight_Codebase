8bdc8f546bb757db2965a92b8249471379f2a6a5
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-28 12:45:05 -0500

    Implemented Concurrent States
    Split state machines into Traversal and Combat to avoid singular state issue

---
+++ IronlightCode/Assets/TESTING/Danish/Controller/CombatStates/TestDanish_CReadyState.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDanish_CReadyState : TestDanish_CombatBaseState
+{
+    TestDanish_Controller_StateManager_v1 stateManager;
+
+    public TestDanish_CReadyState(TestDanish_Controller_StateManager_v1 _Manager) : base(_Manager.gameObject)
+    {
+        stateManager = _Manager;
+    }
+
+
+
+    public override void OnEnter()
+    {
+        Debug.Log("Entering Ready State");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("Exiting Ready State");
+    }
+
+    public override Type Tick()
+    {
+        Debug.Log("Ready State");
+
+        return null;
+    }
+}
---
+++ IronlightCode/Assets/TESTING/Danish/Controller/CombatStates/TestDanish_CombatBaseState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TestDanish_CombatBaseState
+{
+    protected GameObject obj;
+    protected Transform objTransform;
+
+    public TestDanish_CombatBaseState(GameObject gameObject)
+    {
+        this.obj = gameObject;
+        this.objTransform = gameObject.transform;
+    }
+
+    public abstract void OnEnter();
+    public abstract void OnExit();
+    public abstract Type Tick();
+}
