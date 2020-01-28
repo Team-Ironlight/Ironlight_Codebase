dd1e76aa2a4bdca87efdafee31c29310b42b5031
makughini <45440375+makughini@users.noreply.github.com>
2020-01-28 17:42:14 -0500

    Setup For The CheckPoint
    Setup Object and CheckPoint in unity. Nothing change much from the scripts.

--- IronlightCode/Assets/TESTING/CheckPointTesting/CheckPoint.cs
+++ IronlightCode/Assets/TESTING/CheckPointTesting/CheckPoint.cs
@@ -8,7 +8,7 @@ public class CheckPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CheckPointsList = GameObject.FindGameObjectsWithTag("CheckPoint");
     }
 
     // Update is called once per frame
