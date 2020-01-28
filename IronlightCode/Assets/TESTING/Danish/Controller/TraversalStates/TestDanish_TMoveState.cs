744b8a12bd9d98c58aae33dd828848c6898d994d
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-01-28 13:37:59 -0500

    Revert "Revert "Merge pull request #99 from Team-Ironlight/Danish""
    This reverts commit ed23ba12309a9f86760ba6bb5a353e3e1fdf2100.

---
+++ IronlightCode/Assets/TESTING/Danish/Controller/TraversalStates/TestDanish_TMoveState.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDanish_TMoveState : TestDanish_TraversalBaseState
+{
+    TestDanish_Controller_StateManager_v1 stateManager;
+
+    public TestDanish_TMoveState(TestDanish_Controller_StateManager_v1 _Manager) : base(_Manager.gameObject)
+    {
+        stateManager = _Manager;
+    }
+
+
+
+    public override void OnEnter()
+    {
+        Debug.Log("Entering Move State");
+    }
+
+    public override void OnExit()
+    {
+        Debug.Log("Exiting Move State");
+    }
+
+    public override Type Tick()
+    {
+        Debug.Log("Move State");
+
+        if (!stateManager.isMoving)
+        {
+            return typeof(TestDanish_TIdleState);
+        }
+
+        return null;
+    }
+}
