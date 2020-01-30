e43ed81d4c5e1b55f0834361d14f3e06cfe3fac0
WolfHandss <48457883+WolfHandss@users.noreply.github.com>
2020-01-29 21:12:20 -0500

    bug fixes

--- IronlightCode/Assets/TESTING/Iman/RadialTestArea/PLY_ImanBlastTest.cs
+++ IronlightCode/Assets/TESTING/Iman/RadialTestArea/PLY_ImanBlastTest.cs
@@ -131,28 +131,42 @@ public class PLY_ImanBlastTest : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R) || Input.GetMouseButtonDown(0))
         {
-            Visual = Instantiate(ChargeVisual, transform.position, transform.rotation);
+            KeyPressed();
         }
         //while key is held add to radius if it hasnt reached max radius
         if ((Input.GetKey(KeyCode.R) || Input.GetMouseButton(0)) && !coroutineOn)
         {
-            if (chargeCount < radiusMax)
-            {
-                chargeCount += radiusChargeSpeed * Time.deltaTime;
-                Visual.transform.localScale = new Vector3(chargeCount, chargeCount, chargeCount) / 5;
-            }
-            Visual.transform.position = transform.position;
+            KeyHeld();
         }
         //on key released call the blast coroutine with the blast radius calculated
         if (Input.GetKeyUp(KeyCode.R) || Input.GetMouseButtonUp(0))
         {
-            StartCoroutine(RadialAction(chargeCount));
-            coroutineOn = true;
-            Destroy(Visual);
-            Visual = null;
+            KeyReleased();
         }
     }
-
+    //for when key is pressed
+    public void KeyPressed()
+    {
+        Visual = Instantiate(ChargeVisual, transform.position, transform.rotation);
+    }
+    // for when key is held
+    public void KeyHeld()
+    {
+        if (chargeCount < radiusMax)
+        {
+            chargeCount += radiusChargeSpeed * Time.deltaTime;
+            Visual.transform.localScale = new Vector3(chargeCount, chargeCount, chargeCount) / 5;
+        }
+        Visual.transform.position = transform.position;
+    }
+    // for when key is released
+    public void KeyReleased()
+    {
+        StartCoroutine(RadialAction(chargeCount));
+        coroutineOn = true;
+        Destroy(Visual);
+        Visual = null;
+    }
 
     //function to do damage based on distance from the player
     private void CalcDmg(GameObject enemy)
