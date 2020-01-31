40da06d38ab1cc1624285e81a054de2c9191bd79
WolfHandss <48457883+WolfHandss@users.noreply.github.com>
2020-01-31 15:59:19 -0500

    dash

---
+++ IronlightCode/Assets/TESTING/Iman/PlayerDash/PLY_Dash.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLY_Dash : MonoBehaviour
+{
+    [SerializeField] private float dashForce;
+    [SerializeField] private float dashDuration;
+    [HideInInspector] public bool InputRecievced;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash());
+            InputRecievced = true;
+        }
+        else
+        {
+            InputRecievced = false;
+        }
+    }
+
+    IEnumerator Dash()
+    {
+        rb.AddForce(transform.forward * dashForce, ForceMode.VelocityChange);
+
+        yield return new WaitForSeconds(dashDuration);
+
+        rb.velocity = Vector3.zero;
+    }
+}
