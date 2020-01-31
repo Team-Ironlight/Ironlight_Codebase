188871115d968635a746ff402667aa6f72381850
Rhonielvp <48013641+Rhonielvp@users.noreply.github.com>
2020-01-31 16:59:55 -0500

    crystal pieces update

---
+++ IronlightCode/Assets/TESTING/Danish/Crystals/TestDanish_CrystalCollisions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDanish_CrystalCollisions : MonoBehaviour
+{
+    public TestDanish_RotateCrystal crystal;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            crystal.playerCanActivate = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            crystal.playerCanActivate = false;
+        }
+    }
+}
