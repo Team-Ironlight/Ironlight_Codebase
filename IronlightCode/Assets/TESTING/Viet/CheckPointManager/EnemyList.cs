001ce6d67bdb745252589d7f26ce6223668e9c80
makughini <45440375+makughini@users.noreply.github.com>
2020-02-10 15:40:10 -0500

    Remove Old CheckPoint Scripts
    Remove CheckPoint Scrpts that don't use and set up for checkpoint manager.

---
+++ IronlightCode/Assets/TESTING/Viet/CheckPointManager/EnemyList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyList : MonoBehaviour
+{
+    public static GameObject[] enemyList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyList = GameObject.FindGameObjectsWithTag("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
