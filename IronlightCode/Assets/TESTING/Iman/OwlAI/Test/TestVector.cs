c5e077345e1d5bf5fdd6f60bb89f11829ccb6757
Rhon <rhoniel.vp16@hotmail.com>
2020-02-14 00:28:37 -0500

    Finish Menu Time: 11:36 Date: 02-13-2020
    Set everything up for menu to work and change the picture to what we need.

--- IronlightCode/Assets/TESTING/Iman/OwlAI/Test/TestVector.cs
+++ IronlightCode/Assets/TESTING/Iman/OwlAI/Test/TestVector.cs
@@ -8,6 +8,9 @@ public class TestVector : MonoBehaviour
 
     public int YPos; 
     public int GroundPos;
+    public Vector3 startPos;
+
+    public Vector3 SweepEndPos;
 
     Vector3 owlpos;
     // Start is called before the first frame update
@@ -19,9 +22,9 @@ public class TestVector : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(calculate(), transform.position) > 0.1)
+        if (Vector3.Distance(startPos, transform.position) > 0.1)
         {
-            var direction = calculate() - transform.position;
+            var direction = startPos - transform.position;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 3 * Time.deltaTime);
             //move forward
             transform.Translate(0, 0, Time.deltaTime * 4);
@@ -31,21 +34,31 @@ public class TestVector : MonoBehaviour
             var direction = Player.transform.position - transform.position;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 3 * Time.deltaTime);
         }
+        calculateEndAttackPos();
+        calculate();
     }
 
-    Vector3 calculate()
+    void calculate()
     {
         owlpos = transform.position;
         owlpos.y = Player.transform.position.y;
         Vector3 dist = -(Player.transform.position - owlpos);
         dist = dist.normalized * GroundPos;
-        dist.y = Player.transform.position.y + YPos;;
-        //transform.position = dist;
-        return dist;
+        dist = dist + Player.transform.position;
+        dist.y = Player.transform.position.y + YPos;
+        startPos = dist;
+    }
+
+    private void calculateEndAttackPos()
+    {
+        var PPos = Player.transform.position;
+        PPos.y = transform.position.y;
+        SweepEndPos = ((PPos - transform.position).normalized * GroundPos) + Player.transform.position;
+        SweepEndPos.y = Player.transform.position.y + YPos;
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.DrawLine(transform.position, Player.transform.position);
+        Gizmos.DrawLine(transform.position, SweepEndPos);
     }
 }
