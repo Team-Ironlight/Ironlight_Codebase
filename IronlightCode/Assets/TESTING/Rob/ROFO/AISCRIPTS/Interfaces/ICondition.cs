dbb604bb3ea96c66291d78e1a6e51cd7eb419799
Rofosaurus <robertanthonyfowlie@gmail.com>
2020-02-18 19:01:53 -0500

    Namespace shit

---
+++ IronlightCode/Assets/TESTING/Rob/ROFO/AISCRIPTS/Interfaces/ICondition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ROFO
+{
+    public abstract class ICondition : MonoBehaviour
+    {
+        //variables
+
+        //methods
+        public abstract bool Check();
+    }
+}
+
---
+++ IronlightCode/Assets/TESTING/Rob/ROFO/AISCRIPTS/States/Attacks/SnakeRattle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ROFO
+{
+    public class SnakeRattle : IState
+    {
+        public string name = "SnakeRattle";
+
+        public override string Name()
+        {
+            return name;
+        }
+
+
+
+        public override void Enter()
+        {
+
+        }
+
+        public override void Execute(Transform t)
+        {
+            Debug.Log("Rattle");
+        }
+
+        public override void Exit()
+        {
+
+        }
+    }
+}
