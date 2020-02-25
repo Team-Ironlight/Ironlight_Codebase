30dff1bf5c4209cd0163012d79a25c211b0fde84
A00425164 <40704271+A00425164@users.noreply.github.com>
2020-02-25 17:13:57 -0500

    Viet's missing Scripts

---
+++ IronlightCode/Assets/TESTING/Viet/New Folder/DamageComp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageComp : MonoBehaviour
+{
+    public float damageValue; 
+
+    private GameObject attacker = null; //attacker currently empty
+   
+
+    void Update ()
+    {
+
+    }
+
+    void processDmg() // Access the health value of the defender and deal dmg or recover hp by the attacker dmg or heal by amount of heal source
+    {
+
+    }
+
+    void resetValue() // aftter take damage from dmg component, reset the dmg value receive and attacker value
+    {
+        damageValue = 0;
+        attacker = null;
+    }
+}
