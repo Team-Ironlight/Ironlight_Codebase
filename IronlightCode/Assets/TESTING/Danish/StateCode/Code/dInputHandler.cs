cfee81e362e9dc9b83aa03e20982ec681abac5f7
WolfHandss <48457883+WolfHandss@users.noreply.github.com>
2020-02-28 14:12:29 -0500

    k

--- IronlightCode/Assets/TESTING/Danish/StateCode/Code/dInputHandler.cs
+++ IronlightCode/Assets/TESTING/Danish/StateCode/Code/dInputHandler.cs
@@ -38,11 +38,11 @@ namespace Danish.StateCode
         {
             controls = new TestDanish_Controller_Input();
         }
-        private void Awake()
-        {
-            controls = new TestDanish_Controller_Input();
+        //private void Awake()
+        //{
+        //    controls = new TestDanish_Controller_Input();
 
-        }
+        //}
 
         private void Update()
         {
