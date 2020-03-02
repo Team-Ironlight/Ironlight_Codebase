26771d4be36b68570c8af2d60cae9dddfb42bd29
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-03-02 16:30:27 -0500

    completed initial implementation of attacks into stateMachine

--- IronlightCode/Assets/TESTING/Mohamad/Refactor_Attacks/R_BlastAttack.cs
+++ IronlightCode/Assets/TESTING/Mohamad/Refactor_Attacks/R_BlastAttack.cs
@@ -1,14 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Danish.Tools;
 
 namespace Sharmout.attacks
 {
     public class R_BlastAttack
     {
-        public void Init()
-        {
+        Transform muzzleRef = null;
+        dObjectPooler blastPool = null;
 
+        GameObject currentBlast = null;
+        R_BlastLogic logic = null;
+
+        public void Init(Transform _muzzle, dObjectPooler _pool)
+        {
+            muzzleRef = _muzzle;
+            blastPool = _pool;
+        }
+
+        public void StartBlast()
+        {
+            if(currentBlast == null)
+            {
+                currentBlast = GetBlastToShoot();
+                if(currentBlast.TryGetComponent(out R_BlastLogic _logic))
+                {
+                    logic = _logic;
+                }
+            }
+        }
+
+        public void ResetBlast()
+        {
+            if(currentBlast != null)
+            {
+                currentBlast = null;
+                logic = null;
+
+            }
+        }
+
+
+        public void Tick()
+        {
+            logic?.Tick(muzzleRef.position);
+        }
+
+        public void Launch()
+        {
+            logic?.OnRelease();
+        }
+
+        GameObject GetBlastToShoot()
+        {
+            return blastPool.SpawnFromPool("Blast", muzzleRef.position, muzzleRef.rotation);
         }
     }
 }
