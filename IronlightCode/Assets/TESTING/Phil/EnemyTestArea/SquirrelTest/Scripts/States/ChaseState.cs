3ba5b23fc909675ba99da03a5c7bc048d302962d
PhilJamesLapuz <Phil-James_Lapuz@outlook.com>
2020-03-02 16:17:26 -0500

    implement Tiny's HP Damage
    HP Damage for Tiny

--- IronlightCode/Assets/TESTING/Phil/EnemyTestArea/SquirrelTest/Scripts/States/ChaseState.cs
+++ IronlightCode/Assets/TESTING/Phil/EnemyTestArea/SquirrelTest/Scripts/States/ChaseState.cs
@@ -23,6 +23,7 @@ public class ChaseState : Phil_StateMa.BaseState
 
     [Header("Target")]
 	private Transform _mTarget;
+    private GameObject _oTarget;
     [Header("Decision Making")]
     public string OnEnemyLostState = "ChaseState";                                               //To Do:  Convert this to enum
     private string OnEnemyChaseDistance = "ChaseState";                                         //To Do:  Convert this to enum
@@ -51,7 +52,16 @@ public class ChaseState : Phil_StateMa.BaseState
     public override void  OnEnter(MonoBehaviour runner)                                                             // This is called before the first frame
     {
         _mRunner = runner;
-        _mTarget = GameObject.FindWithTag("Player").transform;
+        // _mTarget = GameObject.FindWithTag("Player").transform;
+        _oTarget = GameObject.FindWithTag("Player").gameObject;
+        if (_oTarget != null)
+        {
+            _mTarget = _oTarget.transform;
+        }
+        else
+        {
+            Debug.Log("No Player game objects found in the 'Scene'");
+        }
         _navMeshAgent = runner.GetComponent<NavMeshAgent>();
         _aniMator = runner.GetComponent<Animator>();
         _updateMinMax = runner.GetComponent<AI_AbilityManager>();
