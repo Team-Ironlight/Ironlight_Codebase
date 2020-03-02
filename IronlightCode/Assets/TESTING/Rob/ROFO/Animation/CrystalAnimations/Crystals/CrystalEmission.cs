9fc311547da45cb11e814c70b7b9a61038d56b9f
Rofosaurus <robertanthonyfowlie@gmail.com>
2020-03-02 18:49:39 -0500

    Crystal Animation and shader stuff

---
+++ IronlightCode/Assets/TESTING/Rob/ROFO/Animation/CrystalAnimations/Crystals/CrystalEmission.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalEmission : MonoBehaviour
+{
+    public Material mat;
+    public string collisionTag;
+    public string emissionString;
+    public float emissionMax = 1f;
+    public float speed = 1f;
+    private float emissionCurrent = 0f;
+
+    private void Start()
+    {        
+        mat.SetFloat(emissionString, emissionCurrent);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == collisionTag)
+        {
+            TurnOn();
+        }        
+    }
+
+    private bool isOn = false;
+    private Coroutine c = null;
+    //method to call for emission to go from 0 to set value
+    public void TurnOn()
+    {
+        Debug.Log("Turn ON");
+        if(isOn == false)
+        {
+            Debug.Log("Worked");
+            isOn = true;
+            c = StartCoroutine(On());
+        }
+    }
+
+    IEnumerator On()
+    {
+        while(emissionCurrent < emissionMax)
+        {
+            emissionCurrent += speed * Time.deltaTime;
+            mat.SetFloat(emissionString, emissionCurrent);
+            //Debug.Log(mat.GetFloat("_EmissionIntensity"));
+            yield return null;
+        }
+
+        emissionCurrent = emissionMax;
+        mat.SetFloat(emissionString, emissionCurrent);       
+    }
+}
--- IronlightCode/Assets/TESTING/Rob/ROFO/Tools/CustomCollider.cs
+++ IronlightCode/Assets/TESTING/Rob/ROFO/Tools/CustomCollider.cs
@@ -2,93 +2,101 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//put outside class so accessible to all classes
-public enum ColliderType { sphere, box, capsule };
-public enum CapsuleDirection { Xaxis, Yaxis, Zaxis }
-
-
-//Rob
-//Custom editor gives a handle to any object that has this script
-//Than on game start it will create a sphere collider the same size of the handler
-public class CustomCollider : MonoBehaviour
+namespace ROFO
 {
-    [Header("Handles")]
-    [SerializeField] public bool handleOn = true;
-    [SerializeField] public float handleSize = 1f;
-
-    [SerializeField] public ColliderType currentCollider;
-
-    [Header("Sphere")]
-    [SerializeField] public float radiusSize = 1f;
-    [Header("Box")]
-    [SerializeField] public float length = 1f;
-    [SerializeField] public float width = 1f;
-    [SerializeField] public float depth = 1f;
-    [Header("Capsule")]
-    [SerializeField] public float radius = 1f;
-    [SerializeField] public float height = 1f;
-
-    [SerializeField] public CapsuleDirection direction = CapsuleDirection.Xaxis;
+    //put outside class so accessible to all classes
+    public enum ColliderType { sphere, box, capsule };
+    public enum CapsuleDirection { Xaxis, Yaxis, Zaxis }
 
 
-    [SerializeField] public bool isTrigger = true;
-
-    private Collider newCollider;
-
-
-    private void Start()
+    //Rob
+    //Custom editor gives a handle to any object that has this script
+    //Than on game start it will create a sphere collider the same size of the handler
+    public class CustomCollider : MonoBehaviour
     {
-        //create collider using settings
-        if (currentCollider == ColliderType.sphere)
+        [Header("Handles")]
+        [SerializeField] public bool handleOn = true;
+        [SerializeField] public float handleSize = 1f;
+
+        [SerializeField] public ColliderType currentCollider;
+
+        [Header("Sphere")]
+        [SerializeField] public float radiusSize = 1f;
+        [Header("Box")]
+        [SerializeField] public float length = 1f;
+        [SerializeField] public float width = 1f;
+        [SerializeField] public float depth = 1f;
+        [Header("Capsule")]
+        [SerializeField] public float radius = 1f;
+        [SerializeField] public float height = 1f;
+
+        [SerializeField] public CapsuleDirection direction = CapsuleDirection.Xaxis;
+        [SerializeField] public Vector3 offset = Vector3.zero;
+
+        [SerializeField] public bool isTrigger = true;
+
+        private Collider newCollider;
+
+
+        private void Start()
         {
-            //create new sphere collider
-            newCollider = new SphereCollider();
-            newCollider = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
-
-            //set sphere colliders radius to size of handleSize
-            newCollider.GetComponent<SphereCollider>().radius = radiusSize;
-
-            if (isTrigger)
+            //create collider using settings
+            if (currentCollider == ColliderType.sphere)
             {
-                //set to trigger
-                newCollider.isTrigger = true;
+                //create new sphere collider
+                newCollider = new SphereCollider();
+                newCollider = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
+
+                //set sphere colliders radius to size of handleSize
+                SphereCollider temp = newCollider.GetComponent<SphereCollider>();
+                temp.radius = radiusSize;
+                temp.center = offset;
+
+                if (isTrigger)
+                {
+                    //set to trigger
+                    newCollider.isTrigger = true;
+                }
             }
-        }
-        else if (currentCollider == ColliderType.box)
-        {
-            //create new sphere collider
-            newCollider = new BoxCollider();
-            newCollider = gameObject.AddComponent(typeof(BoxCollider)) as BoxCollider;
-
-            //set box collider dimensions...
-            newCollider.GetComponent<BoxCollider>().size = new Vector3(width, depth, length);
-
-            if (isTrigger)
+            else if (currentCollider == ColliderType.box)
             {
-                //set to trigger
-                newCollider.isTrigger = true;
+                //create new sphere collider
+                newCollider = new BoxCollider();
+                newCollider = gameObject.AddComponent(typeof(BoxCollider)) as BoxCollider;
+
+                //set box collider dimensions...
+                BoxCollider temp = newCollider.GetComponent<BoxCollider>();
+                temp.size = new Vector3(width, depth, length);
+                temp.center = offset;
+
+                if (isTrigger)
+                {
+                    //set to trigger
+                    newCollider.isTrigger = true;
+                }
             }
-        }
-        else if (currentCollider == ColliderType.capsule)
-        {
-            //create new sphere collider
-            newCollider = new CapsuleCollider();
-            newCollider = gameObject.AddComponent(typeof(CapsuleCollider)) as CapsuleCollider;
-
-            //set box collider dimensions...
-            CapsuleCollider temp = GetComponent<CapsuleCollider>();
-            temp.radius = radius;
-            temp.height = height;
-            temp.direction = (int)direction;
-
-            if (isTrigger)
+            else if (currentCollider == ColliderType.capsule)
             {
-                //set to trigger
-                newCollider.isTrigger = true;
-            }
-        }
+                //create new sphere collider
+                newCollider = new CapsuleCollider();
+                newCollider = gameObject.AddComponent(typeof(CapsuleCollider)) as CapsuleCollider;
 
-        //destroy script after???
-        Destroy(this);
+                //set box collider dimensions...
+                CapsuleCollider temp = GetComponent<CapsuleCollider>();
+                temp.radius = radius;
+                temp.height = height;
+                temp.direction = (int)direction;
+                temp.center = offset;
+
+                if (isTrigger)
+                {
+                    //set to trigger
+                    newCollider.isTrigger = true;
+                }
+            }
+
+            //destroy script after???
+            Destroy(this);
+        }
     }
 }
