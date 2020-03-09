2bd00690ca337163bd98cf1edf2cc70062b56073
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2020-03-09 18:36:08 -0400

    refactored light source and dash
    dash goes in input direction or forward if no input.
    refactored light source code to use the Spirt and Health system

---
+++ IronlightCode/Assets/TESTING/Danish/HealthSystemTest/dHealingLight.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Danish.Components;
+
+public class dHealingLight : MonoBehaviour
+{
+    [SerializeField] private Light lightSource = null;
+    [SerializeField] private dSpiritSystem PlayerSpirit = null;
+
+    public float spiritToAdd = 5;
+
+    public float startLightIntensity = 0;
+    public float currentLightIntensity = 0;
+
+    public float depletedLight = 0;
+
+    public bool PlayerInLight = false;
+
+    void Start()
+    {
+        lightSource = gameObject.GetComponentInChildren<Light>();
+
+        startLightIntensity = lightSource.intensity;
+    }
+
+    void Update()
+    {
+        currentLightIntensity = lightSource.intensity;
+
+        if (PlayerInLight)
+        {
+            if(PlayerSpirit.currentSpirit < PlayerSpirit.maxSpirit || PlayerSpirit.currentHealth < PlayerSpirit.maxHealth)
+            {
+                DepleteLightSource();
+            }
+
+            else
+            {
+                Debug.Log("Health and Spirit maxed");
+                RegenerateLightSource();
+            }
+
+        }
+        else
+        {
+            RegenerateLightSource();
+        }
+    }
+
+
+    void RegenerateLightSource()
+    {
+        if(currentLightIntensity == startLightIntensity)
+        {
+            return;
+        }
+
+        if(currentLightIntensity < startLightIntensity)
+        {
+            currentLightIntensity += (spiritToAdd * Time.deltaTime);
+            lightSource.intensity = currentLightIntensity;
+        }
+
+        if(currentLightIntensity > startLightIntensity)
+        {
+            currentLightIntensity = startLightIntensity;
+            lightSource.intensity = currentLightIntensity;
+        }
+
+    }
+
+    void DepleteLightSource()
+    {
+        if(currentLightIntensity == 0)
+        {
+            return;
+        }
+
+        if(currentLightIntensity > 0)
+        {
+            currentLightIntensity -= (spiritToAdd * Time.deltaTime);
+            lightSource.intensity = currentLightIntensity;
+            PlayerSpirit?.GAIN.DoIt(spiritToAdd * Time.deltaTime, 1);
+        }
+
+        if(currentLightIntensity < 0)
+        {
+            currentLightIntensity = 0;
+            lightSource.intensity = currentLightIntensity;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(other.gameObject.TryGetComponent(out dSpiritSystem dSpirit))
+            {
+                PlayerSpirit = dSpirit;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(currentLightIntensity > 0)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                PlayerInLight = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PlayerInLight = false;
+            PlayerSpirit = null;
+        }
+    }
+}
