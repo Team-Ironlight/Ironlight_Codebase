f478593744da6db60b92e506c2a94addeb47ca8e
Baran <brian.baran@torontofilmschool.ca>
2020-03-09 18:58:39 -0400

    AudioVisualFineTuning CreditStart

---
+++ IronlightCode/Assets/TESTING/Brian/Credits/MoveCredits.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MoveCredits : MonoBehaviour
+{
+    Text CurrText;
+    public Text nextText;
+    Vector2 MoveUp;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrText = GetComponent<Text>();
+        MoveUp = CurrText.transform.position;
+        nextText.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TextScroll();
+        if (CurrText.transform.position.y >= 50)
+        {
+            nextText.enabled = true;
+        }
+    }
+    void TextScroll()
+    {
+        CurrText.transform.Translate(transform.up * Time.deltaTime * speed);
+    }
+}
