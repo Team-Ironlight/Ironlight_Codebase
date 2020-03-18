173da4188bb19915be12f500e3fb3662d9112fc0
A00425164 <40704271+A00425164@users.noreply.github.com>
2020-03-18 18:05:38 -0400

    Found Tyler's Scripts

---
+++ IronlightCode/Assets/TESTING/Tyler/Tyler/Scripts/Menu/UI_MainMenuHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_MainMenuHandler : MonoBehaviour
+{  
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);     
+    } 
+    
+    public void Option()
+    {
+        SceneManager.LoadScene(2);     
+    } 
+    
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
