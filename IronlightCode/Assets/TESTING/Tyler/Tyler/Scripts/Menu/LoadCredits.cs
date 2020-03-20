578e2b2b86a7b68f8bb46fe89927d947bc55373f
Rhon <rhoniel.vp16@hotmail.com>
2020-03-20 01:26:22 -0400

    FinalBuild
    We did IT!

---
+++ IronlightCode/Assets/TESTING/Tyler/Tyler/Scripts/Menu/LoadCredits.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadCredits : MonoBehaviour
+{
+
+    public void Credits()
+    {       
+
+        SceneManager.LoadScene(2);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+         
+            Credits();
+        }
+    }
+
+
+}
